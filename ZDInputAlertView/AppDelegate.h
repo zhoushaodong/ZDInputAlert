8cfa1e297d2378d8920fc48772b3707798fc958b
Linghao <943303779@qq.com>
2017-06-20 15:44:15 +0800

    Initial Commit

---
+++ ZDInputAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZDInputAlertView
+//
+//  Created by mac on 2017/6/20.
+//  Copyright © 2017年 com.zhouzhaodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
